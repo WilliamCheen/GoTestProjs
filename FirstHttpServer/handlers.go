77a862262ae540be44c3cf955f7bd763bfdaeeb5
WilliamCheen <williamshaochen@gmail.com>
2020-04-17 09:28:53 +0800

    Change handle file for supperot POST request

--- FirstHttpServer/handlers.go
+++ FirstHttpServer/handlers.go
@@ -3,8 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -16,6 +14,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
@@ -28,30 +27,50 @@ func TodoCreate(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintln(w, "解析错误", err)
 	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	username := r.PostFormValue("username")
 
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-	if err != nil {
-		panic(err)
-	}
+	// body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	if err := r.Body.Close(); err != nil {
 		panic(err)
 	}
-	if err := json.Unmarshal(body, &todo); err != nil {
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(422)
-		if err := json.NewEncoder(w).Encode(err); err != nil {
-			panic(err)
-		}
 
+	fmt.Printf("拿到了用户名===》" + username)
+	if len(username) > 0 {
+		fmt.Printf("拿到了用户名")
 		t := RepoCreateTodoWithName(todo, username)
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(http.StatusCreated)
+		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(t); err != nil {
 			panic(err)
 		}
+	} else {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		http.Error(w, "用户名不能为空", 422)
 	}
+
+	/*
+		if err := json.Unmarshal(body, &todo); err != nil {
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.WriteHeader(422)
+			if err := json.NewEncoder(w).Encode(err); err != nil {
+				panic(err)
+			}
+
+			t := RepoCreateTodoWithName(todo, username)
+			w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.WriteHeader(http.StatusCreated)
+
+			if err := json.NewEncoder(w).Encode(t); err != nil {
+				panic(err)
+			}
+		}*/
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
